a937a0767fc4bf725f6f78132b2d6629383a8924
Chinenye <83243717+Birdie13-stack@users.noreply.github.com>
2023-10-02 00:05:18 -0700

    Updated Functionality

--- public/extension/content.js
+++ public/extension/content.js
@@ -1,1 +1,55 @@
 console.log("I have been injected");
+
+var recorder = null;
+
+function onAccessApproved() {
+  recorder = new MediaRecorder(stream);
+  recorder.start();
+
+  recorder.onstop = function () {
+    stream.getTracks().forEach(function (track) {
+      if (track.readyState === "live") {
+        track.stop();
+      }
+    });
+  };
+
+  recorder.ondataavailable = function (event) {
+    let recordedBlob = event.data;
+    let url = URL.createObjectURL(recordedBlob);
+
+    let a = document.createElement("a");
+
+    a.href = url;
+    a.style.display = none;
+    a.download = "recording.webm";
+
+    document.body.appendChild(a);
+    a.click();
+
+    documents.body.removeChild(a);
+
+    URL.revokeObjectURL(url);
+  };
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "request_recording") {
+    console.log("requesting recording");
+
+    sendResponse(`processed: ${message.action}`);
+
+    navigator.mediaDevices
+      .getDisplayMedia({
+        audio: true,
+        video: {
+          width: 999999999999,
+          height: 999999999999,
+        },
+      })
+
+      .then((stream) => {
+        onAccessApproved(stream);
+      });
+  }
+});
--- public/extension/js/popup.js
+++ public/extension/js/popup.js
@@ -1,4 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   const startButton = document.getElementById("start");
   const endButton = document.getElementById("end");
+
+  startButton.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "stopvideo" },
+        function (response) {
+          if (!chrome.runtime.lastError) {
+            console.log(response);
+          } else {
+            console.log(chrome.runtime.lastError, "error error!!");
+          }
+        }
+      );
+    });
+  });
+
+  endButton.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "request_recording" },
+        function (response) {
+          if (!chrome.runtime.lastError) {
+            console.log(response);
+          } else {
+            console.log(chrome.runtime.lastError, "error error!!");
+          }
+        }
+      );
+    });
+  });
 });
